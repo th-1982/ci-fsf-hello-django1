0d9691a5db6ce7ac2f9f24c767b5af3ca26a2efa
th-1982 <>
2023-10-27 18:21:30 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['th-1982-django-todo-app-a60f53410194.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL=postgres://ksanykgm:sWQV59ubTSNPlo1C_FzkGheJyeJPpxLY@tyke.db.elephantsql.com/ksanykgm'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 }
     
